75e2ec2615926352790dd73d409a683a2f1dabd9
stanislawczykj <jakubstanislawczyk98@gmail.com>
2021-03-07 21:43:37 +0100

    Added missing unit tests

--- src/services/token.service.spec.ts
+++ src/services/token.service.spec.ts
@@ -1,11 +1,17 @@
 import {expect} from 'chai';
 import {TokenService} from './token.service';
 import {User} from '../models/entities/user';
-import {fullUser} from '../../test/fixtures/user.fixture';
+import {fullUser, user} from '../../test/fixtures/user.fixture';
 import bcrypt from 'bcrypt';
 import {LoginCredentials} from '../models/common/login-credentials';
 import {AuthenticationError} from 'apollo-server';
 import {JwtUtils} from '../../test/utils/common/jwt.utils';
+import {UserRole} from '../enums/user-role';
+import {JwtToken} from '../models/common/jwt-token';
+import jwt, {TokenExpiredError} from 'jsonwebtoken';
+import {classToPlain} from 'class-transformer';
+import config from 'config';
+import {InvalidTokenError} from '../models/errors/invalid-token.error';
 
 context('TokenService', () => {
 
@@ -47,6 +53,101 @@ context('TokenService', () => {
       expect(() => tokenService.getUserToken(loginCredentials, user))
         .to.throw('Authentication data are not valid')
         .and.to.be.an.instanceOf(AuthenticationError);
+    });
+  });
+
+  describe('validateToken', () => {
+    it('should validate token', () => {
+      // Arrange
+      const authenticatedUser: User = {
+        ...user,
+        role: UserRole.ADMIN,
+      };
+      const jwtSecret: string = config.get('security.jwt.secret');
+      const allowedRoles: UserRole[] = [UserRole.ADMIN];
+      const jwtToken: JwtToken = new JwtToken(authenticatedUser);
+      const token: string = jwt.sign(
+        classToPlain(jwtToken),
+        jwtSecret,
+        {
+          expiresIn: 100000,
+        }
+      );
+
+      // Act
+      const isValidated: boolean = tokenService.validateToken(token, allowedRoles);
+
+      // Assert
+      expect(isValidated).to.be.true;
+    });
+
+    it('should not validate token if it expires', () => {
+      // Arrange
+      const jwtSecret: string = config.get('security.jwt.secret');
+      const allowedRoles: UserRole[] = [UserRole.ADMIN];
+      const authenticatedUser: User = {
+        ...user,
+        role: UserRole.ADMIN,
+      };
+      const jwtToken: JwtToken = new JwtToken(authenticatedUser);
+      const token: string = jwt.sign(
+        classToPlain(jwtToken),
+        jwtSecret,
+        {
+          expiresIn: -10000,
+        }
+      );
+
+      // Act & Assert
+      expect(() => tokenService.validateToken(token, allowedRoles))
+        .to.throw('jwt expired')
+        .and.to.be.an.instanceOf(TokenExpiredError);
+    });
+
+    it('should not validate token if is not supported role', () => {
+      // Arrange
+      const jwtSecret: string = config.get('security.jwt.secret');
+      const authenticatedUser: User = {
+        ...user,
+        role: 'NotSupportedRole' as UserRole,
+      };
+      const allowedRoles: UserRole[] = [UserRole.USER];
+      const jwtToken: JwtToken = new JwtToken(authenticatedUser);
+      const token: string = jwt.sign(
+        classToPlain(jwtToken),
+        jwtSecret,
+        {
+          expiresIn: 100000,
+        }
+      );
+
+      // Act & Assert
+      expect(() => tokenService.validateToken(token, allowedRoles))
+        .to.throw(`Given role "${authenticatedUser.role}" is not supported`)
+        .and.to.be.an.instanceOf(InvalidTokenError);
+    });
+
+    it('should not validate token if role is not allowed', () => {
+      // Arrange
+      const jwtSecret: string = config.get('security.jwt.secret');
+      const authenticatedUser: User = {
+        ...user,
+        role: UserRole.ADMIN,
+      };
+      const allowedRoles: UserRole[] = [UserRole.USER];
+      const jwtToken: JwtToken = new JwtToken(authenticatedUser);
+      const token: string = jwt.sign(
+        classToPlain(jwtToken),
+        jwtSecret,
+        {
+          expiresIn: 100000,
+        }
+      );
+
+      // Act & Assert
+      expect(() => tokenService.validateToken(token, allowedRoles))
+        .to.throw('User is not required to perform this action')
+        .and.to.be.an.instanceOf(InvalidTokenError);
     });
   });
 });
