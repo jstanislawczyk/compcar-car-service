326ab32cc5e27cfa8468eb65f8e76f7796ded031
Jakub Stanisławczyk <jakubstanislawczyk98@gmail.com>
2021-03-18 21:10:47 +0100

    Health check (#5)

---
+++ src/services/health.service.ts
@@ -0,0 +1,37 @@
+import {Service} from 'typedi';
+import {InjectRepository} from 'typeorm-typedi-extensions';
+import {UserRepository} from '../repositories/user.repository';
+import {HealthStatus} from '../enums/health-status';
+import {ApplicationHealth} from '../models/types/health/application-health';
+
+@Service()
+export class HealthService {
+
+  constructor(
+    @InjectRepository()
+    private readonly userRepository: UserRepository,
+  ) {
+  }
+
+  public async getApplicationHealth(): Promise<ApplicationHealth> {
+    const applicationHealth: ApplicationHealth = new ApplicationHealth();
+
+    applicationHealth.healthDetails.database.mysql = await this.getMysqlDatabaseHealthStatus();
+    applicationHealth.generalStatus = await this.getGeneralApplicationHealthStatus(applicationHealth);
+
+    return applicationHealth;
+  }
+
+  private async getMysqlDatabaseHealthStatus(): Promise<HealthStatus> {
+    try {
+      await this.userRepository.findOne();
+      return HealthStatus.OK;
+    } catch (error) {
+      return HealthStatus.CRITICAL;
+    }
+  }
+
+  private getGeneralApplicationHealthStatus(applicationHealth: ApplicationHealth): HealthStatus {
+    return applicationHealth.healthDetails.database.mysql;
+  }
+}
