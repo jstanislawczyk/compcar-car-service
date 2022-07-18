c677f7a7e2dee8e34b2e2252257e9ba5b8a03faa
jstanislawczyk <jakubstanislawczyk98@gmail.com>
2022-07-19 00:02:40 +0200

    Changed user name

--- config/default.js
+++ config/default.js
@@ -9,7 +9,7 @@ module.exports = {
     mysql: {
         url: process.env.MYSQL_URL || 'localhost',
         port: Number(process.env.MYSQL_PORT) || 3306,
-        username: process.env.MYSQL_USERNAME || 'root',
+        username: process.env.MYSQL_USERNAME || 'compcar_root',
         password: process.env.MYSQL_PASSWORD || 'root',
         database: process.env.MYSQL_DATABASE || 'compcar',
         logging: process.env.MYSQL_LOGGING === 'true' || false,
