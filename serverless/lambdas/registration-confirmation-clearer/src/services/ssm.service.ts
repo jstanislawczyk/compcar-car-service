29b95a39af7faed6bfeb70a6cb8ad7fa68a0914e
jstanislawczyk <jakubstanislawczyk98@gmail.com>
2022-09-28 18:17:59 +0200

    Fixed lambda build

--- serverless/lambdas/registration-confirmation-clearer/src/services/ssm.service.ts
+++ serverless/lambdas/registration-confirmation-clearer/src/services/ssm.service.ts
@@ -1,22 +1,25 @@
 import {SSM} from 'aws-sdk';
-import {Logger} from '../../../../../src/common/logger';
 import {GetParameterRequest, GetParameterResult} from 'aws-sdk/clients/ssm';
 
 export class SsmService {
 
+  constructor(
+    private readonly ssm: SSM = new SSM(),
+  ) {
+  }
+
   public async getParameter(name: string, withDecryption: boolean = true): Promise<string | undefined> {
-    const ssm: SSM = new SSM();
     const params: GetParameterRequest = {
       Name: name,
       WithDecryption: withDecryption,
     };
 
     try {
-      const getParameterResult: GetParameterResult = await ssm.getParameter(params).promise();
+      const getParameterResult: GetParameterResult = await this.ssm.getParameter(params).promise();
 
       return getParameterResult.Parameter?.Value;
     } catch (error: any) {
-      Logger.log(`KMS decryption failed. Error: ${error.message}`);
+      console.log(`KMS decryption failed. Error: ${error.message}`);
       throw error;
     }
   }
